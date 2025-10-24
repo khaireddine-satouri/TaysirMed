26c30342164a4a4853b5dd22c82db2439bf2fafc
kheireddine satouri <73032240+khaireddine-satouri@users.noreply.github.com>
2025-10-24 13:21:19 +0200

    Updated EditSeanceModal.tsx

--- src/components/soignant/EditSeanceModal.tsx
+++ src/components/soignant/EditSeanceModal.tsx
@@ -1,6 +1,6 @@
 // src/components/EditSeanceModal.tsx
 import { useEffect, useMemo, useState } from "react";
-import { supabase, Seance, UserBase } from "../lib/supabase";
+import { supabase, Seance, UserBase } from "../../lib/supabase";
 import { useAuth } from "../contexts/AuthContext";
 import { X, Trash2, AlertTriangle } from "lucide-react";
 
