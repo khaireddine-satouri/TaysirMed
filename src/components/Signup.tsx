3b1a751ff59adaa0e0806b554268659b11e5d24a
kheireddine satouri <73032240+khaireddine-satouri@users.noreply.github.com>
2025-10-25 08:27:48 +0200

    Updated Signup.tsx

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -72,7 +72,7 @@ export default function Signup({ onSwitchToLogin }: { onSwitchToLogin: () => voi
             {/* Nom */}
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-2">
-                Nom <span className="text-gray-500">*</span>
+                Nom <span className="text-gray-600">*</span>
               </label>
               <input
                 type="text"
