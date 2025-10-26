039dd068c30c360ee30e36069a7b0f54f6c04afd
kheireddine satouri <73032240+khaireddine-satouri@users.noreply.github.com>
2025-10-26 11:32:15 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import './index.css';
-
-/**
- * Si vous avez un provider de cache sécurisé (ex: SecureCacheProvider),
- * c’est ici qu’il faut le monter autour de l’app.
- * L’exemple ci-dessous le montre en commentaire. Dé-commentez quand vous aurez ajouté ce provider.
- */
-// import { SecureCacheProvider } from './crypto/SecureCacheContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    {/* <SecureCacheProvider> */}
-      <App />
-    {/* </SecureCacheProvider> */}
+    <App />
   </StrictMode>
 );
